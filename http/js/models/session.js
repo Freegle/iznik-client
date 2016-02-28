099cdebe2a1aa496ecfd13b32c81d0f03568d32f
Edward Hibbert <edward@ehibbert.org.uk>
2016-02-28 18:42:13 +0000

    Reduce number of session calls

--- http/js/models/session.js
+++ http/js/models/session.js
@@ -3,16 +3,12 @@ Iznik.Models.Session = IznikModel.extend({
 
     playBeep: false,
 
+    loggedIn: false,
+    lastTested: null,
+
     notificationsSetup: false,
 
     initialize: function() {
-    },
-
-    updateCounts: function() {
-        this.testLoggedIn();
-    },
-
-    testLoggedIn: function() {
         var self = this;
 
         // We don't want to beep if we are visible.
@@ -23,179 +19,201 @@ Iznik.Models.Session = IznikModel.extend({
         $(document).on('show', function() {
             self.playBeep = false;
         });
+    },
 
-        $.ajax({
-            url: API + 'session',
-            success: function(ret) {
-                if ((ret.ret == 0)) {
-                    //console.log("Logged in");
-                    self.set(ret);
+    updateCounts: function() {
+        this.testLoggedIn();
+    },
 
-                    if (!self.notificationsSetup) {
-                        // Set up service worker for push notifications
-                        self.notificationsSetup = true;
+    testLoggedIn: function() {
+        var self = this;
 
-                        if ('serviceWorker' in navigator) {
-                            // Add a rand to the service worker to stop it being cached and therefore not picking
-                            // up fixes.
-                            navigator.serviceWorker.register('/js/iznik/sw.js?' + Math.random()).then(function(reg) {
-                                console.log(':^)', reg);
-                                reg.pushManager.subscribe({
-                                    userVisibleOnly: true
-                                }).then(function(sub) {
-                                    console.log('endpoint:', sub.endpoint);
-                                    var p = sub.endpoint.lastIndexOf('/');
-                                    var subscription = sub.endpoint;
-                                    var me = Iznik.Session.get('me');
-                                    if (me) {
-                                        if (me.notifications && me.notifications.push && me.notifications.push.subscription == subscription) {
-                                            console.log("Already got our permissions");
-                                        } else {
-                                            // We don't currently have this
-                                            console.log("Not got permissions; save", sub.endpoint, me.notifications.push.subscription);
-                                            var type = 'Google';
-                                            var key = null;
-                                            if (subscription.indexOf('services.mozilla.com') !== -1) {
-                                                type = 'Firefox';
-                                            }
-                                            Iznik.Session.save({
-                                                id: me.id,
-                                                notifications: {
-                                                    push: {
-                                                        type: type,
-                                                        subscription: subscription
-                                                    }
+        console.log("testLoggedIn"); console.trace();
+        var now = moment().valueOf();
+
+        // Only query the server if it's the first time (perhaps since we loggged out) or it's been a while.  This
+        // reduces fairly expensive session calls.
+        if (!self.lastTested || now - self.lastTested > 10000) {
+            $.ajax({
+                url: API + 'session',
+                success: function(ret) {
+                    self.lastTested = moment().valueOf();
+                    if ((ret.ret == 0)) {
+                        //console.log("Logged in");
+                        self.set(ret);
+
+                        if (!self.notificationsSetup) {
+                            // Set up service worker for push notifications
+                            self.notificationsSetup = true;
+
+                            if ('serviceWorker' in navigator) {
+                                // Add a rand to the service worker to stop it being cached and therefore not picking
+                                // up fixes.
+                                navigator.serviceWorker.register('/js/iznik/sw.js?' + Math.random()).then(function(reg) {
+                                    console.log(':^)', reg);
+                                    reg.pushManager.subscribe({
+                                        userVisibleOnly: true
+                                    }).then(function(sub) {
+                                        console.log('endpoint:', sub.endpoint);
+                                        var p = sub.endpoint.lastIndexOf('/');
+                                        var subscription = sub.endpoint;
+                                        var me = Iznik.Session.get('me');
+                                        if (me) {
+                                            if (me.notifications && me.notifications.push && me.notifications.push.subscription == subscription) {
+                                                console.log("Already got our permissions");
+                                            } else {
+                                                // We don't currently have this
+                                                console.log("Not got permissions; save", sub.endpoint, me.notifications.push.subscription);
+                                                var type = 'Google';
+                                                var key = null;
+                                                if (subscription.indexOf('services.mozilla.com') !== -1) {
+                                                    type = 'Firefox';
                                                 }
-                                            }, {
-                                                patch: true
-                                            });
+                                                Iznik.Session.save({
+                                                    id: me.id,
+                                                    notifications: {
+                                                        push: {
+                                                            type: type,
+                                                            subscription: subscription
+                                                        }
+                                                    }
+                                                }, {
+                                                    patch: true
+                                                });
+                                            }
+                                        }
+                                    });
+                                }).catch(function(err) {
+                                    console.log(':^(', err);
+                                });
+                            }
+                        }
+
+                        // We get an array of groups back - we want it to be a collection.
+                        self.set('groups', new IznikCollection(ret.groups));
+
+                        // We may also get an array of modconfigs.
+                        if (ret.configs) {
+                            self.set('configs', new IznikCollection(ret.configs));
+                        }
+
+                        self.loggedIn = true;
+                        self.trigger('isLoggedIn', true);
+
+                        var admin = Iznik.Session.isAdmin();
+                        var support = Iznik.Session.isAdminOrSupport();
+
+                        // Update our various counts.
+                        var counts = [
+                            {
+                                fi: 'pending',
+                                el: '.js-pendingcount',
+                                ev: 'pendingcountschanged',
+                                window: true,
+                                sound: true
+                            },
+                            {
+                                fi: 'spam',
+                                el: '.js-spamcount',
+                                ev: 'spamcountschanged',
+                                window: true,
+                                sound: true
+                            },
+                            {
+                                fi: 'pendingother',
+                                el: '.js-pendingcountother',
+                                ev: 'pendingcountsotherchanged',
+                                window: false,
+                                sound: false
+                            },
+                            {
+                                fi: 'spammembers',
+                                el: '.js-spammemberscount',
+                                ev: 'spammembercountschanged',
+                                window: false,
+                                sound: false
+                            },
+                            {
+                                fi: 'pendingmembers',
+                                el: '.js-pendingmemberscount',
+                                ev: 'pendingmemberscountschanged',
+                                window: true,
+                                sound: true
+                            },
+                            {
+                                fi: 'pendingmembersother',
+                                el: '.js-pendingmemberscountother',
+                                ev: 'pendingmemberscountsotherchanged',
+                                window: false,
+                                sound: false
+                            },
+                            {
+                                fi: 'spammerpendingadd',
+                                el: '.js-spammerpendingaddcount',
+                                ev: 'spammerpendingaddcountschanged',
+                                window: support,
+                                sound: false
+                            },
+                            {
+                                fi: 'spammerpendingremove',
+                                el: '.js-spammerpendingremovecount',
+                                ev: 'spammerpendingremovecountschanged',
+                                window: admin,
+                                sound: false
+                            }
+                        ];
+
+                        var total = 0;
+
+                        _.each(counts, function(count) {
+                            if (ret.work[count.fi]) {
+
+                                if (count.window) {
+                                    total += ret.work[count.fi];
+                                }
+
+                                var countel = $(count.el);
+                                var currcount = countel.html();
+                                countel.html(ret.work[count.fi]);
+
+                                if (ret.work[count.fi] > currcount || currcount == 0) {
+                                    // Only trigger this when the counts increase.  This will pick up new messages
+                                    // without screen flicker due to re-rendering when we're processing messages and
+                                    // deleting them.  There's a minor timing window where a message could arrive as
+                                    // one is deleted, leaving the counts the same, but this will resolve itself when
+                                    // our current count drops to zero, or worst case when we refresh.
+                                    Iznik.Session.trigger(count.ev);
+
+                                    if (ret.work[count.fi] > 0 && count.sound) {
+                                        var settings = Iznik.Session.get('me').settings;
+
+                                        if (presdef('playbeep', settings, 1) && self.playBeep) {
+                                            var sound = new Audio("/sounds/alert.wav");
+                                            sound.play();
                                         }
                                     }
-                                });
-                            }).catch(function(err) {
-                                console.log(':^(', err);
-                            });
-                        }
-                    }
-
-                    // We get an array of groups back - we want it to be a collection.
-                    self.set('groups', new IznikCollection(ret.groups));
-
-                    // We may also get an array of modconfigs.
-                    if (ret.configs) {
-                        self.set('configs', new IznikCollection(ret.configs));
-                    }
-
-                    self.trigger('isLoggedIn', true);
-
-                    var admin = Iznik.Session.isAdmin();
-                    var support = Iznik.Session.isAdminOrSupport();
-
-                    // Update our various counts.
-                    var counts = [
-                        {
-                            fi: 'pending',
-                            el: '.js-pendingcount',
-                            ev: 'pendingcountschanged',
-                            window: true,
-                            sound: true
-                        },
-                        {
-                            fi: 'spam',
-                            el: '.js-spamcount',
-                            ev: 'spamcountschanged',
-                            window: true,
-                            sound: true
-                        },
-                        {
-                            fi: 'pendingother',
-                            el: '.js-pendingcountother',
-                            ev: 'pendingcountsotherchanged',
-                            window: false,
-                            sound: false
-                        },
-                        {
-                            fi: 'spammembers',
-                            el: '.js-spammemberscount',
-                            ev: 'spammembercountschanged',
-                            window: false,
-                            sound: false
-                        },
-                        {
-                            fi: 'pendingmembers',
-                            el: '.js-pendingmemberscount',
-                            ev: 'pendingmemberscountschanged',
-                            window: true,
-                            sound: true
-                        },
-                        {
-                            fi: 'pendingmembersother',
-                            el: '.js-pendingmemberscountother',
-                            ev: 'pendingmemberscountsotherchanged',
-                            window: false,
-                            sound: false
-                        },
-                        {
-                            fi: 'spammerpendingadd',
-                            el: '.js-spammerpendingaddcount',
-                            ev: 'spammerpendingaddcountschanged',
-                            window: support,
-                            sound: false
-                        },
-                        {
-                            fi: 'spammerpendingremove',
-                            el: '.js-spammerpendingremovecount',
-                            ev: 'spammerpendingremovecountschanged',
-                            window: admin,
-                            sound: false
-                        }
-                    ];
-
-                    var total = 0;
-
-                    _.each(counts, function(count) {
-                        if (ret.work[count.fi]) {
-
-                            if (count.window) {
-                                total += ret.work[count.fi];
-                            }
-
-                            var countel = $(count.el);
-                            var currcount = countel.html();
-                            countel.html(ret.work[count.fi]);
-
-                            if (ret.work[count.fi] > currcount || currcount == 0) {
-                                // Only trigger this when the counts increase.  This will pick up new messages
-                                // without screen flicker due to re-rendering when we're processing messages and
-                                // deleting them.  There's a minor timing window where a message could arrive as
-                                // one is deleted, leaving the counts the same, but this will resolve itself when
-                                // our current count drops to zero, or worst case when we refresh.
-                                Iznik.Session.trigger(count.ev);
-
-                                if (ret.work[count.fi] > 0 && count.sound) {
-                                    var settings = Iznik.Session.get('me').settings;
-
-                                    if (presdef('playbeep', settings, 1) && self.playBeep) {
-                                        var sound = new Audio("/sounds/alert.wav");
-                                        sound.play();
-                                    }
                                 }
+                            } else {
+                                $(count.el).empty();
                             }
-                        } else {
-                            $(count.el).empty();
-                        }
-                    })
+                        })
 
-                    document.title = (total == 0) ? 'ModTools' : ('(' + total + ') ModTools');
-                } else {
+                        document.title = (total == 0) ? 'ModTools' : ('(' + total + ') ModTools');
+                    } else {
+                        self.loggedIn = false;
+                        self.trigger('isLoggedIn', false);
+                    }
+                },
+                error: function() {
+                    console.log("Get settings failed");
+                    self.lastTested = moment().valueOf();
+                    self.loggedIn = false;
                     self.trigger('isLoggedIn', false);
                 }
-            },
-            error: function() {
-                console.log("Get settings failed");
-                self.trigger('isLoggedIn', false);
-            }
-        })
+            })
+        } else {
+            self.trigger('isLoggedIn', self.loggedIn);
+        }
     },
 
     forceLogin: function() {
