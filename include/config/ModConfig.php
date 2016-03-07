9d76d5967e1a32c074de0a2d2f1a96cdd58183f0
Edward Hibbert <edward@ehibbert.org.uk>
2016-03-07 16:38:00 +0000

    CC fix

--- include/config/ModConfig.php
+++ include/config/ModConfig.php
@@ -348,6 +348,7 @@ class ModConfig extends Entity
                 $ret = NULL;
         }
 
+        error_log("Get BCC for {$this->modconfig['name']} $action = $ret");
         return($ret);
     }
 
