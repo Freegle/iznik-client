7832e4a6146c67466fbb56b6ef84fdd5c508dbbd
Edward Hibbert <edward@ehibbert.org.uk>
2016-03-31 07:22:36 +0100

    Move stranded message purging out of scheduler

--- scripts/cron/purge_messages.php
+++ scripts/cron/purge_messages.php
@@ -14,7 +14,7 @@ $dbhm = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
     PDO::ATTR_EMULATE_PREPARES => FALSE
 ));
 
-# Delete old non-Freegle messages
+# Purge old non-Freegle messages
 $start = date('Y-m-d', strtotime("midnight 31 days ago"));
 error_log("Purge non-Freegle before $start");
 
@@ -34,13 +34,31 @@ do {
 
 error_log("Deleted $total");
 
-# Now delete messages which have been deleted - we keep them for a while for PD purposes.
+# Now purge messages which have been deleted - we keep them for a while for PD purposes.
 $start = date('Y-m-d', strtotime("midnight 2 days ago"));
 error_log("Purge deleted messages before $start");
 $total = 0;
 
 do {
     $sql = "SELECT messages.id FROM messages WHERE deleted IS NOT NULL AND deleted <= '$start' LIMIT 1000;";
+    $msgs = $dbhm->query($sql)->fetchAll();
+    foreach ($msgs as $msg) {
+        $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
+        $total++;
+
+        if ($total % 1000 == 0) {
+            error_log("...$total");
+        }
+    }
+} while (count($msgs) > 0);
+
+# Now purge messages which are stranded, not on any groups.
+$start = date('Y-m-d', strtotime("midnight 2 days ago"));
+error_log("Purge stranded messages before $start");
+$total = 0;
+
+do {
+    $sql = "SELECT messages.id FROM messages WHERE arrival <= '$start' AND id NOT IN (SELECT DISTINCT msgid FROM messages_groups) LIMIT 1000;";
     $msgs = $dbhm->query($sql)->fetchAll();
     foreach ($msgs as $msg) {
         $dbhm->exec("DELETE FROM messages WHERE id = {$msg['id']};");
