5714251f6612e80808d1be9c706be731b03d39ad
Edward Hibbert <edward@ehibbert.org.uk>
2016-05-19 10:30:15 +0100

    Message delete sync to FD broken

--- scripts/migrate/migrate_fd_delete.php
+++ scripts/migrate/migrate_fd_delete.php
@@ -13,11 +13,18 @@ $dbhold = new PDO($dsn, $dbconfig['user'], $dbconfig['pass'], array(
     PDO::ATTR_EMULATE_PREPARES => FALSE
 ));
 
-$msgs = $dbhr->preQuery("SELECT messageid, nameshort FROM messages INNER JOIN messages_groups ON messages.id = messages_groups.msgid AND messages_groups.deleted = 1 AND messages_groups.collection = 'Approved' INNER JOIN groups ON groups.id = messages_groups.groupid;");
+$msgs = $dbhr->preQuery("SELECT messageid, nameshort, message FROM messages INNER JOIN messages_groups ON messages.id = messages_groups.msgid AND messages_groups.deleted = 1 AND messages_groups.collection = 'Approved' INNER JOIN groups ON groups.id = messages_groups.groupid;");
 error_log("Check " . count($msgs));
 
 foreach ($msgs as $msg) {
-    $sql = "SELECT uniquemsg, id, groupid, subject FROM messages WHERE messageid = " . $dbhold->quote("<{$msg['messageid']}>") . " OR messageid = " . $dbhold->quote($msg['messageid']) . ";";
+    # Get message ID - it will have been cleared in the deleted message.
+    if (preg_match('/^Message-ID\:(.*?)$/mi', $msg['message'], $matches)) {
+        $msgid = $matches[1];
+        $msgid = str_replace('<', '', $msgid);
+        $msgid = str_replace('>', '', $msgid);
+        $msgid = trim($msgid);
+    }
+    $sql = "SELECT uniquemsg, id, groupid, subject FROM messages WHERE messageid = " . $dbhold->quote("<$msgid>") . " OR messageid = " . $dbhold->quote($msgid) . ";";
     $msgs2 = $dbhold->query($sql);
     foreach ($msgs2 as $msg2) {
         error_log("#{$msg2['uniquemsg']} {$msg2['id']},{$msg2['groupid']} {$msg2['subject']}");
