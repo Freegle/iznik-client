95d2116ab1fb1878e7318eecd338b57ae034e4d2
Edward Hibbert <edward@ehibbert.org.uk>
2016-06-09 15:06:51 +0100

    Speed ups via SQL query caching in redis Make plugin work with FF 47.

--- test/ut/php/include/groupTest.php
+++ test/ut/php/include/groupTest.php
@@ -244,7 +244,8 @@ class groupTest extends IznikTestCase {
         error_log(__METHOD__);
 
         $dbconfig = array (
-            'host' => '127.0.0.1',
+            'host' => SQLHOST,
+            'port' => SQLPORT,
             'user' => SQLUSER,
             'pass' => SQLPASSWORD,
             'database' => SQLDB
