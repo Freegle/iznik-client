66cf5761e31653624b1e6852523eda9d9922584c
Edward Hibbert <edward@ehibbert.org.uk>
2016-07-01 07:43:58 +0100

    Chats not shown/hidden according to modtools flag

--- include/chat/ChatRoom.php
+++ include/chat/ChatRoom.php
@@ -239,7 +239,7 @@ class ChatRoom extends Entity
         # The chats we can see are:
         # - either for a group (possibly a modonly one)
         # - a conversation between two users that we have not closed
-        $sql = "SELECT chat_rooms.* FROM chat_rooms LEFT JOIN chat_roster ON chat_roster.userid = ? AND chat_rooms.id = chat_roster.chatid WHERE ((groupid IN (SELECT groupid FROM memberships WHERE userid = ?) $modtoolsq) OR user1 = ? OR user2 = ?) AND (status IS NULL OR status != ?);";
+        $sql = "SELECT chat_rooms.* FROM chat_rooms LEFT JOIN chat_roster ON chat_roster.userid = ? AND chat_rooms.id = chat_roster.chatid WHERE ((groupid IN (SELECT groupid FROM memberships WHERE userid = ?) OR user1 = ? OR user2 = ?)) $modtoolsq AND (status IS NULL OR status != ?);";
         #error_log($sql . var_export([ $userid, $userid, $userid, $userid, ChatRoom::STATUS_CLOSED ], TRUE));
         $rooms = $this->dbhr->preQuery($sql, [ $userid, $userid, $userid, $userid, ChatRoom::STATUS_CLOSED ]);
         foreach ($rooms as $room) {
