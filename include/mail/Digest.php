abe1a76c04680fc8c062a3756dd2af31389ff27a
Edward Hibbert <edward@ehibbert.org.uk>
2016-07-20 16:44:49 +0100

    Mailoff should turn mail off in FD too (for now)

--- include/mail/Digest.php
+++ include/mail/Digest.php
@@ -55,6 +55,24 @@ class Digest
         $g = new Group($this->dbhr, $this->dbhm, $groupid);
         $groupname = $g->getPublic()['namedisplay'];
 
+        # TODO This code will die once we move over.
+        global $dbconfig;
+        $dsnfd = "mysql:host={$dbconfig['host']};dbname=republisher;charset=utf8";
+
+        $dbhfd = new PDO($dsnfd, $dbconfig['user'], $dbconfig['pass'], array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => FALSE
+        ));
+
+        $emails = $u->getEmails();
+        foreach ($emails as $email) {
+            $email = $email['email'];
+            $sql = "UPDATE users SET digest = 0 WHERE useremail LIKE " . $dbhfd->quote($email) . " OR groupsemail LIKE " . $dbhfd->quote($email) . ";";
+            #error_log("$sql");
+            $dbhfd->exec($sql);
+        }
+        # TODO end
+
         $this->log->log([
             'type' => Log::TYPE_USER,
             'subtype' => Log::SUBTYPE_MAILOFF,
