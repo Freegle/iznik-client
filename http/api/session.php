4119a1e8aec16874abf9c5f4ef3d04810282f2f8
Edward Hibbert <edward@ehibbert.org.uk>
2016-09-06 23:42:54 +0100

    Can't log in when using a legacy link

--- http/api/session.php
+++ http/api/session.php
@@ -103,7 +103,25 @@ function session() {
             $f = NULL;
             $ret = array('ret' => 1, 'status' => 'Invalid login details');
 
-            if ($action) {
+            if ($fblogin) {
+                # We've been asked to log in via Facebook.
+                $f = new Facebook($dbhr, $dbhm);
+                list ($session, $ret) = $f->login();
+                /** @var Session $session */
+                $id = $session ? $session->getUserId() : NULL;
+            } else if ($yahoologin) {
+                # Yahoo.
+                $y = Yahoo::getInstance($dbhr, $dbhm);
+                list ($session, $ret) = $y->login($returnto);
+                /** @var Session $session */
+                $id = $session ? $session->getUserId() : NULL;
+            } else if ($googlelogin) {
+                # Google
+                $g = new Google($dbhr, $dbhm, $mobile);
+                list ($session, $ret) = $g->login($googleauthcode);
+                /** @var Session $session */
+                $id = $session ? $session->getUserId() : NULL;
+            } else if ($action) {
                 switch ($action) {
                     case 'LostPassword': {
                         $id = $user->findByEmail($email);
@@ -139,24 +157,6 @@ function session() {
                         $u->setPrivate('publishconsent', 1);
                     }
                 }
-            } else if ($fblogin) {
-                # We've been asked to log in via Facebook.
-                $f = new Facebook($dbhr, $dbhm);
-                list ($session, $ret) = $f->login();
-                /** @var Session $session */
-                $id = $session ? $session->getUserId() : NULL;
-            } else if ($yahoologin) {
-                # Yahoo.
-                $y = Yahoo::getInstance($dbhr, $dbhm);
-                list ($session, $ret) = $y->login($returnto);
-                /** @var Session $session */
-                $id = $session ? $session->getUserId() : NULL;
-            } else if ($googlelogin) {
-                # Google
-                $g = new Google($dbhr, $dbhm, $mobile);
-                list ($session, $ret) = $g->login($googleauthcode);
-                /** @var Session $session */
-                $id = $session ? $session->getUserId() : NULL;
             }
 
             if ($id) {
