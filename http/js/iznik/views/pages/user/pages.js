0a22e26e78e7691f02ababc89cace448e4188bc8
Edward Hibbert <edward@ehibbert.org.uk>
2016-09-09 11:37:22 +0100

    Add map on settings to show where postcode is.

--- http/js/iznik/views/pages/user/pages.js
+++ http/js/iznik/views/pages/user/pages.js
@@ -84,6 +84,16 @@ define([
                 }, success: function(ret) {
                     if (ret.ret == 0) {
                         self.recordLocation(ret.locations[0], true);
+
+                        // Update our map if we have one.
+                        var map = self.$('.js-locmap');
+                        if (map.length > 0) {
+                            var width = self.$('.js-postcode').width();
+                            map.css('width', width);
+                            map.css('height', width);
+                            var mapicon = window.location.protocol + '//' + window.location.hostname + '/images/mapmarker.gif';
+                            map.html('<img class="img-thumbnail" src="https://maps.google.com/maps/api/staticmap?size=' + width + 'x' + width + '&zoom=12&center=' + ret.locations[0].lat + ','  + ret.locations[0].lng + '&maptype=roadmap&markers=icon:' + mapicon + '|' + ret.locations[0].lat + ','  + ret.locations[0].lng + '&sensor=true" />');
+                        }
                     }
                 }
             });
