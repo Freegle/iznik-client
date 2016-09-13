40c3681628a3a8bf985bb034b14ab71e83426ddc
Edward Hibbert <edward@ehibbert.org.uk>
2016-09-13 21:26:08 +0100

    Showing pending events for non-mods

--- include/group/CommunityEvent.php
+++ include/group/CommunityEvent.php
@@ -67,7 +67,7 @@ class CommunityEvent extends Entity
         $ctxq = $ctx ? " AND end > '{$ctx['end']}' " : '';
 
         $mysqltime = date("Y-m-d H:i:s", time());
-        $sql = "SELECT communityevents.id, communityevents_dates.end FROM communityevents INNER JOIN communityevents_groups ON communityevents_groups.eventid = communityevents.id AND groupid IN (SELECT groupid FROM memberships WHERE userid = ?) AND deleted = 0 INNER JOIN communityevents_dates ON communityevents_dates.eventid = communityevents.id AND end >= ? $pendingq $ctxq ORDER BY end ASC LIMIT 20;";
+        $sql = "SELECT communityevents.id, communityevents_dates.end FROM communityevents INNER JOIN communityevents_groups ON communityevents_groups.eventid = communityevents.id AND groupid IN (SELECT groupid FROM memberships WHERE userid = ? AND role IN ('Owner', 'Moderator')) AND deleted = 0 INNER JOIN communityevents_dates ON communityevents_dates.eventid = communityevents.id AND end >= ? $pendingq $ctxq ORDER BY end ASC LIMIT 20;";
         #error_log("$sql, $userid, $mysqltime");
         $events = $this->dbhr->preQuery($sql, [
             $userid,
