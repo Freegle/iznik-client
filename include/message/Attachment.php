c5c89a9fdeef2e8e6c1dabbc200cdcea3de0b1bb
Edward Hibbert <edward@ehibbert.org.uk>
2016-09-16 12:34:25 +0100

    Don't return null attachments.

--- include/message/Attachment.php
+++ include/message/Attachment.php
@@ -120,7 +120,7 @@ class Attachment
     }
 
     public function getById($id) {
-        $sql = "SELECT id FROM {$this->table} WHERE {$this->idatt} = ? ORDER BY id;";
+        $sql = "SELECT id FROM {$this->table} WHERE {$this->idatt} = ? AND data IS NOT NULL ORDER BY id;";
         $atts = $this->dbhr->preQuery($sql, [$id]);
         $ret = [];
         foreach ($atts as $att) {
