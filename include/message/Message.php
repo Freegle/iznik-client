906f438ad055c191bfc1ad6be429a8edb078ad16
Edward Hibbert <edward@ehibbert.org.uk>
2016-09-30 08:50:53 +0100

    Add user id and email to BCC

--- include/message/Message.php
+++ include/message/Message.php
@@ -1706,7 +1706,7 @@ class Message
                         FALSE,
                         NULL);
 
-                    $this->mailer($me, TRUE, $this->getFromname(), $bcc, NULL, $name, $g->getModsEmail(), $subject, "(This is a BCC of a message sent to a Freegle Direct user.)\n\n" . $body);
+                    $this->mailer($me, TRUE, $this->getFromname(), $bcc, NULL, $name, $g->getModsEmail(), $subject, "(This is a BCC of a message sent to Freegle Direct user #" . $this->getFromuser() . " $to)\n\n" . $body);
 
                     # We, as a mod, have seen this message - update the roster to show that.  This avoids this message
                     # appearing as unread to us and other mods.
