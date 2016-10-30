c63e2f2674efc7569dd3db245cab959242842052
Edward Hibbert <edward@ehibbert.org.uk>
2016-10-30 09:12:00 +0000

    Facebook notifications and eBay voting

--- scripts/cli/user_facebook_notify.php
+++ scripts/cli/user_facebook_notify.php
@@ -8,7 +8,7 @@ require_once(IZNIK_BASE . '/include/session/Facebook.php');
 
 $opts = getopt('i:m:u:');
 
-if (count($opts) < 3) {
+if (count($opts) < 2) {
     echo "Usage: hhvm user_facebook_notify.php (-i <id of user to notify>) -m <Message to send> -u <URL to link to>\n";
 } else {
     $id = $opts['i'];
@@ -19,7 +19,14 @@ if (count($opts) < 3) {
     $f = new Facebook($dbhr, $dbhm);
 
     $users = $dbhr->preQuery("SELECT * FROM users_logins WHERE type = 'Facebook' $userq ORDER BY lastaccess DESC;");
+    $count = 0;
     foreach ($users as $user) {
         $f->notify($user['uid'], $message, $url);
+
+        $count++;
+
+        if ($count % 1000 === 0) {
+            error_log("...$count / " . count($users));
+        }
     }
 }
