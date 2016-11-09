b0d7d244fe6b237090e4dbdcfc726aa169b34837
Edward Hibbert <edward@ehibbert.org.uk>
2016-11-09 16:48:23 +0000

    Improved chat button display for small screens

--- include/misc/pageheader.php
+++ include/misc/pageheader.php
@@ -198,7 +198,7 @@ require_once(IZNIK_BASE . '/include/misc/scripts.php');
     <link rel="stylesheet" href="/css/dd.css">
     <link rel="stylesheet" href="/css/fileinput.css" />
 
-    <link rel="stylesheet" type="text/css" href="/css/style.css?a=102">
+    <link rel="stylesheet" type="text/css" href="/css/style.css?a=104">
     <!--[if lt IE 9]>
     <link rel="stylesheet" type="text/css" href="/css/ie-only.css">
     <![endif]-->
