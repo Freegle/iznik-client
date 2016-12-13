47dd7356c19cc0bd4511f24313220949ced87928
Edward Hibbert <edward@ehibbert.org.uk>
2016-12-13 10:13:36 +0000

    Route trailing slashes the same as without

--- http/js/iznik/router.js
+++ http/js/iznik/router.js
@@ -40,6 +40,11 @@ define([
                 }
             });
 
+            // Any pages with trailing slashes should route the same as ones without.
+            this.route(/(.*)\/+$/, "trailFix", function (id) {
+                this.navigate(id, true);
+            });
+
             this.bind('route', this.pageView);
         },
 
@@ -1220,8 +1225,6 @@ define([
         },
 
         groupPlugin: function(groupid) {
-            console.log("Group plugin", groupid);
-
             // Might be trailing guff in legacy routes.
             groupid = parseInt(groupid);
             this.userExploreGroup(groupid, true);
