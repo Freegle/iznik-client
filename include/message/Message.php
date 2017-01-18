bd65ddd9ddeee138a72030fe67ab2a4628480e44
Edward Hibbert <edward@ehibbert.org.uk>
2017-01-18 10:03:16 +0000

    Another footer

--- include/message/Message.php
+++ include/message/Message.php
@@ -2422,6 +2422,7 @@ class Message
 
         # Get rid of sigs
         $textbody = preg_replace('/^Get Outlook for Android.*/ms', '', $textbody);
+        $textbody = preg_replace('/^Sent from my phone.*/ms', '', $textbody);
         $textbody = preg_replace('/^Sent from my iPad.*/ms', '', $textbody);
         $textbody = preg_replace('/^Sent from my .*smartphone./ms', '', $textbody);
         $textbody = preg_replace('/^Sent from my iPhone.*/ms', '', $textbody);
