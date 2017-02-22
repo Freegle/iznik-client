dd86d722f6cd73bf26096abe03b2d2d3fd7a24ed
Edward Hibbert <edward@ehibbert.org.uk>
2017-02-22 21:18:22 +0000

    Not able to scroll

--- include/misc/pageheader.php
+++ include/misc/pageheader.php
@@ -287,7 +287,7 @@ require_once(IZNIK_BASE . '/include/misc/scripts.php');
     <link rel="stylesheet" href="/css/dd.css">
     <link rel="stylesheet" href="/css/fileinput.css" />
 
-    <link rel="stylesheet" type="text/css" href="/css/style.css?a=153">
+    <link rel="stylesheet" type="text/css" href="/css/style.css?a=155">
     <!--[if lt IE 9]>
     <link rel="stylesheet" type="text/css" href="/css/ie-only.css">
     <![endif]-->
