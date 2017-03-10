3342b8a16e4794b19386daf285592d8f538d2bb7
Edward Hibbert <edward@ehibbert.org.uk>
2017-03-10 10:11:57 +0000

    Show per-group donation figure

--- include/dashboard/Dashboard.php
+++ include/dashboard/Dashboard.php
@@ -88,6 +88,15 @@ class Dashboard {
             $ret['Outcomes'][$type] = $outcomes;
         }
 
+        if ($groupid) {
+            # Also get the donations this ear.
+            $mysqltime = date("Y-m-d H:i:s", strtotime("midnight 1st January this year"));
+
+            $sql = "SELECT SUM(GrossAmount) AS total FROM users_donations INNER JOIN memberships ON users_donations.userid = memberships.userid AND memberships.groupid = ? WHERE users_donations.timestamp > '$mysqltime';";
+            error_log($sql);
+            $donations = $this->dbhr->preQuery($sql, [ $groupid ]);
+            $ret['donationsthisyear'] = $donations[0]['total'];
+        }
 
         return($ret);
     }
