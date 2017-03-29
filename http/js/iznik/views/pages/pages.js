77a9258534e4cd0b0e4b5dc58a897cce8cccf3f0
Chris Cant <chris@phdcc.com>
2017-03-29 19:57:53 +0100

    Don't show app buttons

--- http/js/iznik/views/pages/pages.js
+++ http/js/iznik/views/pages/pages.js
@@ -147,7 +147,7 @@ define([
                     showNetworkStatus(); // CC
                     if (self.appButtons) {
                         // Show app buttons.
-                        $('#js-appbuttons').show();
+                        // CC $('#js-appbuttons').show();
                     }
 
                     $('#botleft').empty();
