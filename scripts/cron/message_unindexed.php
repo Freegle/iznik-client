03ad7fe02524330378f79adaa8e58fab6dc597fc
Edward Hibbert <edward@ehibbert.org.uk>
2017-03-29 07:42:37 +0100

    Some messages not indexed

--- scripts/cron/message_unindexed.php
+++ scripts/cron/message_unindexed.php
@@ -5,7 +5,7 @@ require_once(IZNIK_BASE . '/include/db.php');
 require_once(IZNIK_BASE . '/include/utils.php');
 require_once(IZNIK_BASE . '/include/message/Message.php');
 
-$msgs = $dbhr->preQuery("SELECT messages.id FROM messages INNER JOIN messages_groups ON messages_groups.msgid = messages.id WHERE messages.id NOT IN (SELECT msgid FROM messages_index) AND messages_groups.deleted = 0 ORDER BY messages_groups.arrival DESC;");
+$msgs = $dbhr->preQuery("SELECT messages.id FROM messages INNER JOIN messages_groups ON messages_groups.msgid = messages.id AND messages_groups.collection = 'Approved' WHERE messages.id NOT IN (SELECT msgid FROM messages_index) AND messages_groups.deleted = 0 ORDER BY messages_groups.arrival DESC;");
 
 foreach ($msgs as $msg) {
     $m = new Message($dbhr, $dbhm, $msg['id']);
