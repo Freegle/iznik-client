dec181484d9fd218544ccaf32c7940055de9919b
Edward Hibbert <edward@ehibbert.org.uk>
2017-04-01 08:41:30 +0100

    A/B test wrong way round

--- http/api/abtest.php
+++ http/api/abtest.php
@@ -12,7 +12,7 @@ function abtest() {
     switch ($_REQUEST['type']) {
         case 'GET': {
             $uid = presdef('uid', $_REQUEST, NULL);
-            $variants = $dbhr->preQuery("SELECT * FROM abtest WHERE uid = ? ORDER BY rate, RAND() DESC;", [
+            $variants = $dbhr->preQuery("SELECT * FROM abtest WHERE uid = ? ORDER BY rate DESC, RAND();", [
                 $uid
             ]);
 
