25d0cce04cfe698db93d2eea52b0df6c80145d7e
Edward Hibbert <edward@ehibbert.org.uk>
2017-04-04 16:38:09 +0100

    Left stats code commented out

--- scripts/cron/group_stats.php
+++ scripts/cron/group_stats.php
@@ -6,13 +6,13 @@ require_once(IZNIK_BASE . '/include/utils.php');
 require_once(IZNIK_BASE . '/include/group/Group.php');
 require_once(IZNIK_BASE . '/include/misc/Stats.php');
 
-//$date = date('Y-m-d', strtotime("yesterday"));
-//$groups = $dbhr->preQuery("SELECT * FROM groups;");
-//foreach ($groups as $group) {
-//    error_log($group['nameshort']);
-//    $s = new Stats($dbhr, $dbhm, $group['id']);
-//    $s->generate($date);
-//}
+$date = date('Y-m-d', strtotime("yesterday"));
+$groups = $dbhr->preQuery("SELECT * FROM groups;");
+foreach ($groups as $group) {
+    error_log($group['nameshort']);
+    $s = new Stats($dbhr, $dbhm, $group['id']);
+    $s->generate($date);
+}
 
 # Find what proportion of overall activity an individual group is responsible for.  We will use this when calculating
 # a fundraising target.
