56f209f130f35ad60c387b649ced0bdcf9f8ecff
Edward Hibbert <edward@ehibbert.org.uk>
2017-05-14 11:08:11 +0100

    Fixes

--- scripts/cron/chat_spam.php
+++ scripts/cron/chat_spam.php
@@ -11,7 +11,7 @@ $uid = $u->findByEmail(MODERATOR_EMAIL);
 
 # We look for users who are not whitelisted, but where we have marked multiple chat messages from them
 # as spam.  Exclude messages automarked as spam.
-$users = $dbhr->preQuery("SELECT DISTINCT chat_messages.userid, COUNT(*) AS count FROM chat_messages LEFT JOIN spam_users ON spam_users.userid = chat_messages.userid INNER JOIN users ON users.id = chat_messages.userid WHERE reviewrejected = 1 AND reviewrejected != $uid (collection IS NULL OR collection != 'Whitelisted') AND systemrole = 'User' GROUP BY chat_messages.userid HAVING count > 5  ORDER BY count DESC;");
+$users = $dbhr->preQuery("SELECT DISTINCT chat_messages.userid, COUNT(*) AS count FROM chat_messages LEFT JOIN spam_users ON spam_users.userid = chat_messages.userid INNER JOIN users ON users.id = chat_messages.userid WHERE reviewrejected = 1 AND reviewrejected != $uid AND (collection IS NULL OR collection != 'Whitelisted') AND systemrole = 'User' GROUP BY chat_messages.userid HAVING count > 5  ORDER BY count DESC;");
 $count = 0;
 
 foreach ($users as $user) {
@@ -32,7 +32,7 @@ foreach ($users as $user) {
         if ($reviews[0]['count'] > 0) {
             error_log("...{$user['userid']} spam count {$user['count']} marked as spam, auto-mark {$reviews[0]['count']} pending review");
             $count += $reviews[0]['count'];
-            $dbhm->preExec("UPDATE chat_messages SET reviewrequired = 0, reviewedby = ? WHERE userid = ? AND reviewrequired = 1 AND reviewedby IS NULL;", [
+            $dbhm->preExec("UPDATE chat_messages SET reviewrequired = 0, reviewrejected = 1, reviewedby = ? WHERE userid = ? AND reviewrequired = 1 AND reviewedby IS NULL;", [
                 $uid,
                 $user['userid']
             ]);
