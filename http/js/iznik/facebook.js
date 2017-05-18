d601c095a74a4e97d818745273debf5dc0325164
Chris Cant <chris@phdcc.com>
2017-05-18 21:47:08 +0100

    merge mobile

--- http/js/iznik/facebook.js
+++ http/js/iznik/facebook.js
@@ -6,73 +6,73 @@ define([
     'iznik/base',
     'iznik/openfb'
 ], function ($, _, Backbone, Iznik) {
-  var facebookAppId = $('meta[name=facebook-app-id]').attr("content");
+    var facebookAppId = $('meta[name=facebook-app-id]').attr("content");
 
-  var tryingFacebookLogin = false;
+    var tryingFacebookLogin = false;
 
-  Iznik.Views.FBLoad = Iznik.View.extend({
-    FBLoaded: false,
-    FBLoading: false,
-    FBDisabled: false,
+    Iznik.Views.FBLoad = Iznik.View.extend({
+        FBLoaded: false,
+        FBLoading: false,
+        FBDisabled: false,
 
-    isDisabled: function () {
-      return this.FBDisabled;
-    },
-    render: function () {
-      var self = this;
-      // console.log("Render FBLoad");
+        isDisabled: function () {
+            return this.FBDisabled;
+        },
+        render: function () {
+            var self = this;
+            // console.log("Render FBLoad");
 
-      if (self.FBLoaded) {
-        // console.log("Already loaded");
-        this.trigger('fbloaded');
-      } else if ((!self.FBLoaded) && (!self.FBLoading)) {
-        // console.log("Load FB API");
-        self.FBLoading = true;
+            if (self.FBLoaded) {
+                // console.log("Already loaded");
+                this.trigger('fbloaded');
+            } else if ((!self.FBLoaded) && (!self.FBLoading)) {
+                // console.log("Load FB API");
+                self.FBLoading = true;
 
-        // The load might fail if we have a blocker.  The only way to deal with this is via a timeout.
-        self.timeout = window.setTimeout(function () {
-            console.error("Facebook API load failed - blocked?");
-            self.FBLoading = false;
-            self.FBLoaded = true;
-            self.FBDisabled = true;
-            $('.js-privacy').show();
-            self.trigger('fbloaded');
-        }, 30000);
+                // The load might fail if we have a blocker.  The only way to deal with this is via a timeout.
+                self.timeout = window.setTimeout(function () {
+                    console.error("Facebook API load failed - blocked?");
+                    self.FBLoading = false;
+                    self.FBLoaded = true;
+                    self.FBDisabled = true;
+                    $('.js-privacy').show();
+                    self.trigger('fbloaded');
+                }, 30000);
 
-        // Load the SDK asynchronously
-        (function (d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s);
-          js.id = id;
-          js.src = "https://connect.facebook.net/en_US/sdk.js";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+                // Load the SDK asynchronously
+                (function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "https://connect.facebook.net/en_US/sdk.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
 
-        window.fbAsyncInit = function () {
-          // console.log("FB asyncInit");
-          self.FBLoading = false;
-          self.FBLoaded = true;
-          clearTimeout(self.timeout);
+                window.fbAsyncInit = function () {
+                    // console.log("FB asyncInit");
+                    self.FBLoading = false;
+                    self.FBLoaded = true;
+                    clearTimeout(self.timeout);
 
-          try {
-            FB.init({
-              appId: facebookAppId,
-              cookie: true,  // enable cookies to allow the server to access the session
-              version: 'v2.2' // use version 2.2
-            });
+                    try {
+                        FB.init({
+                            appId: facebookAppId,
+                            cookie: true,  // enable cookies to allow the server to access the session
+                            version: 'v2.2' // use version 2.2
+                        });
 
-            self.trigger('fbloaded');
-            // console.log("FB Loaded");
-          } catch (e) {
-            console.log("Facebook init failed");
-            console.log(e);
-          }
-        }
-      } else {
-        // console.log("FB still loading...");
-      }
-    },
+                        self.trigger('fbloaded');
+                        // console.log("FB Loaded");
+                    } catch (e) {
+                        console.log("Facebook init failed");
+                        console.log(e);
+                    }
+                }
+            } else {
+                // console.log("FB still loading...");
+            }
+        },
 
         signin: function () {  // CC..
             var self = this;
