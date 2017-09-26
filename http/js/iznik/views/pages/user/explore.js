83ba16a75d25ef6052ea4b2fa4fd7e8715a5139d
Edward Hibbert <edward@ehibbert.org.uk>
2017-09-26 15:57:15 +0100

    * Ensure that Explore map always shows a group

--- http/js/iznik/views/pages/user/explore.js
+++ http/js/iznik/views/pages/user/explore.js
@@ -238,9 +238,12 @@ define([
                     }
                 });
 
+                var groupsshown = 0;
+
                 self.collection.each(function(group) {
                     if (bounds.contains(new google.maps.LatLng(group.get('lat'), group.get('lng'))) &&
                         group.get('onmap') && group.get('publish')) {
+                        groupsshown++;
                         var latLng = new google.maps.LatLng(group.get('lat'), group.get('lng'));
 
                         if (within > 20) {
@@ -305,6 +308,11 @@ define([
                         });
                     }
                 });
+
+                if (groupsshown == 0 && self.map.getZoom() > 0) {
+                    // We aren't showing any groups.  Zoom out until we are.
+                    self.map.setZoom(self.map.getZoom() - 1);
+                }
             }
         },
 
