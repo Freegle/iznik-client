50465687d2b4de440130448becd46962d634258c
Edward Hibbert <edward@ehibbert.org.uk>
2017-10-06 19:04:44 +0100

    Remove error log

--- include/group/Volunteering.php
+++ include/group/Volunteering.php
@@ -90,7 +90,6 @@ class Volunteering extends Entity
             $me = whoAmI($this->dbhr, $this->dbhm);
             if (!$pending || ($me && $me->hasPermission(User::PERM_NATIONAL_VOLUNTEERS))) {
                 $sql = "SELECT NULL AS groupid, volunteering.id, volunteering.pending, volunteering_dates.end, volunteering_dates.applyby FROM volunteering LEFT JOIN volunteering_groups ON volunteering_groups.volunteeringid = volunteering.id AND deleted = 0 AND expired = 0 LEFT JOIN volunteering_dates ON volunteering_dates.volunteeringid = volunteering.id WHERE groupid IS NULL AND deleted = 0 AND expired = 0 $pendingq $ctxq ORDER BY id DESC LIMIT 20;";
-                error_log("Get national $sql");
                 $volunteerings = array_merge($volunteerings, $this->dbhr->preQuery($sql));
 
                 # Sort, as we have added national ones at the end.
