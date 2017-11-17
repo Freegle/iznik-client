f74e7684402c5b76ff77e2c3567bbfd0152c4ca5
Edward Hibbert <edward@ehibbert.org.uk>
2017-11-17 18:05:43 +0000

    Speed-up: inline templates used by a JS file into that same JS file when fetching from the server.  This reduces HTTP requests on the client.

--- http/js/iznik/utility.js
+++ http/js/iznik/utility.js
@@ -1,8 +1,7 @@
 define([
     'jquery',
-    'underscore',
-    'moment'
-], function($, _, moment) {
+    'underscore'
+], function($, _) {
     // TODO Namespace pollution here
     //
     // Ensure we can log.
@@ -309,89 +308,6 @@ function haversineDistance(coords1, coords2, isMiles) {
     if(isMiles) d /= 1.60934;
 
     return d;
-}
-
-// We fetch templates over AJAX, then compile them ready for use.
-function tplName(tpl) {
-    var nm = 'text!/template/' + tpl.replace(/\_/g, '/') + '.html';
-    return(nm);
-}
-
-var loadedTemplates = [];
-
-function templateFetch(tpl) {
-    var promise = new Promise(function(resolve, reject) {
-        require([ tplName(tpl), 'moment' ], function(html, moment) {
-            // Make templates less likely to bomb out with an exception if a variable is undefined, by
-            // using the internal obj.
-            html = html ? html.replace(/\{\{/g, '{{obj.') : null;
-            html = html.replace(/\{\{obj.obj./g, '{{obj.');
-            html = html.replace(/\{\{obj.timeago/g, '{{timeago');
-            //console.log("Updated HTML", html);
-
-            // Use a closure to wrap the underscore template so that if we get an error we can log it
-            // rather than bomb out of the whole javascript.
-            function getClosure(tpl, und) {
-                return (function (obj) {
-                    try {
-                        obj = _.extend(obj, {
-                            // We call this timeago function from within templates.  This allows us to insert a formatted time
-                            // into the HTML before it's added to the DOM, which is more efficient than adding it and then
-                            // manipulating it afterwards.
-                            timeago: function(d) {
-                                var m = (new moment(d));
-
-                                var s = m.fromNow();
-
-                                // Don't allow future times.
-                                s = s.indexOf('in ') === 0 ? 'just now': s;
-
-                                return(s);
-                            },
-                            moment: moment
-                        });
-
-                        html = und(obj);
-
-                        // Sanitise to remove script tags
-                        html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-
-                        // Add template name into the HTML, which is very useful for debugging.
-                        html = "<!-- " + tpl + " -->\r\n" + html;
-                        return html;
-                    } catch (e) {
-                        console.error("Template " + tpl + " expansion failed with " + e.message + ", ");
-                        console.log(this);
-                        console.log(html);
-                        return ('');
-                    }
-                });
-            }
-
-            loadedTemplates[tpl] = getClosure(tpl, _.template(html, {
-                interpolate: /\{\{(.+?)\}\}/g,
-                evaluate: /<%(.+?)%>/g,
-                escape: /<%-(.+?)%>/g,
-            }, {
-                // This supposedly improves performance - see https://jsperf.com/underscore-template-function-with-variable-setting
-                variable: 'obj',
-            }));
-
-            resolve(tpl);
-        }, function(err) {
-            console.log("Require error in", tpl, err);
-        })
-    });
-
-    return(promise);
-}
-
-function resolvedPromise(self) {
-    // Return a resolved promise as there is nothing to do.
-    var p = new Promise(function(resolve, reject) {
-        resolve(self);
-    });
-    return(p);
 }
 
 function getURLParam(name) {
