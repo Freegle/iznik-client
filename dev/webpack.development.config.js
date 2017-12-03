6ddf54c6a496bb050c5cf80e523b941abe552fb9
Nick Sellen <git@nicksellen.co.uk>
2017-12-03 09:44:57 +0000

    Reformat dev/

--- dev/webpack.development.config.js
+++ dev/webpack.development.config.js
@@ -59,7 +59,7 @@ module.exports = new Config().extend('dev/webpack.base.config.js').merge({
     new HtmlWebpackPlugin({
       hash: true,
       template: join(ROOT, 'client/index.ejs'),
-      filename: 'index.html',
+      filename: 'index.html'
     })
   ]
 });
