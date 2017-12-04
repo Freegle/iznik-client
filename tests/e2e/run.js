faff50b2761435b7075314a5383826fcfa503409
Nick Sellen <git@nicksellen.co.uk>
2017-12-04 00:42:52 +0000

    Use glob matching for e2e tests

--- tests/e2e/run.js
+++ tests/e2e/run.js
@@ -1,5 +1,6 @@
 const createTestCafe = require('testcafe')
 const { join } = require('path')
+const glob = require('glob-promise');
 
 const isCI = process.argv.includes('--ci')
 const browsers = isCI ? ['chrome:headless'] : ['chromium'];
@@ -8,8 +9,7 @@ const browsers = isCI ? ['chrome:headless'] : ['chromium'];
   const testcafe = await createTestCafe('localhost')
   const runner = await testcafe.createRunner()
   const failedCount = await runner
-    // should pass in test files via args or use some autodiscovery
-    .src(join(__dirname, 'findStuff.js'))
+    .src(await glob(join(__dirname, '**/*.e2e.js')))
     .browsers(browsers)
     .screenshots('e2e-screenshots')
     .run({
