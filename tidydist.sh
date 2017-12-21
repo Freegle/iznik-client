6f304ee17a08f1bdce08ccc2c07a1af29ed8a279
Chris Cant <chris@phdcc.com>
2017-12-21 18:32:20 +0000

    Add tidy script

---
+++ tidydist.sh
@@ -0,0 +1,12 @@
+cd dist
+#rm stats.json report.html
+rm -r apple*
+rm images/streetwhack.png
+mv *.png css
+mv *.woff css
+mv *.woff2 css
+mv *.ttf css
+mv *.eot css
+mv *.svg css
+mv *.jpg css
+cd ..
