fafb9828c298d9939bcd5820d1f8d122a394b6a1
Chris Cant <chris@phdcc.com>
2017-12-22 21:43:42 +0000

    update version date

--- dev/webpack.appfd.config.js
+++ dev/webpack.appfd.config.js
@@ -69,8 +69,8 @@ module.exports = new Config().extend({
     }),
     // CC new FaviconsPlugin('images/user_logo.png'),
     new webpack.DefinePlugin({
-      APP_VERSION: JSON.stringify('1.3.0, 21 December 2017'),
-      // CC SET AT END: API: JSON.stringify(BASE_URL+'/api/'),
+      APP_VERSION: JSON.stringify('1.3.0, 22 December 2017'),
+      // CC SET ABOVE: API: JSON.stringify(BASE_URL+'/api/'),
       FACEBOOK_APPID: JSON.stringify('134980666550322'),
       FACEBOOK_GRAFFITI_APPID: JSON.stringify('115376591981611'),
       GOOGLE_CLIENT_ID: JSON.stringify(
