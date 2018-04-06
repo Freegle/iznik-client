3bbbeb6894bab15830ec883e6f19ec094ea924d5
chriscant <chris@phdcc.com>
2018-04-06 10:54:32 +0100

    New dev environment

--- dev/webpack.appmt.config.js
+++ dev/webpack.appmt.config.js
@@ -69,7 +69,7 @@ module.exports = new Config().extend({
     }),
     // CC new FaviconsPlugin('images/modtools_logo.png'),
     new webpack.DefinePlugin({
-      APP_VERSION: JSON.stringify('0.1.3, 22 February 2018.'),
+      APP_VERSION: JSON.stringify('0.1.4, 4 April 2018.'),
       // CC SET ABOVE: API: JSON.stringify(BASE_URL+'/api/'),
       FACEBOOK_APPID: JSON.stringify('134980666550322'),
       FACEBOOK_GRAFFITI_APPID: JSON.stringify('115376591981611'),
