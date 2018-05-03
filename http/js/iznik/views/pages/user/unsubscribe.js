64592da0e52b87c8e22e730ebb92640d9a242d7c
Edward Hibbert <edward@ehibbert.org.uk>
2018-05-03 14:14:13 +0100

    * GDPR: Add ability for a member to wipe their personal data.

--- http/js/iznik/views/pages/user/unsubscribe.js
+++ http/js/iznik/views/pages/user/unsubscribe.js
@@ -11,7 +11,34 @@ define([
 
         events: {
             'click .js-leave': "leave",
-            'click .js-findme': 'findme'
+            'click .js-findme': 'findme',
+            'click .js-forget': 'forget'
+        },
+
+        forget: function() {
+            var self = this;
+
+            var v = new Iznik.Views.Confirm();
+            v.template = 'user_unsubscribe_confirm';
+
+            self.listenToOnce(v, 'confirmed', function() {
+                var p = Iznik.Session.forget();
+
+                p.then(function() {
+                    (new Iznik.Views.User.Pages.Unsubscribe.Forgotten()).render();
+                });
+
+                p.catch(function(ret) {
+                    var v = new Iznik.Views.User.Pages.Unsubscribe.ForgetFail({
+                        model: new Iznik.Model({
+                            status: ret.status
+                        })
+                    });
+                    v.render();
+                });
+            });
+
+            v.render();
         },
 
         findme: function() {
@@ -114,4 +141,11 @@ define([
         }
     });
 
+    Iznik.Views.User.Pages.Unsubscribe.Forgotten = Iznik.Views.Modal.extend({
+        template: 'user_unsubscribe_forgotten'
+    });
+
+    Iznik.Views.User.Pages.Unsubscribe.ForgetFail = Iznik.Views.Modal.extend({
+        template: 'user_unsubscribe_forgetfail'
+    });
 });
