ed2ad0ffa03cccae18e42c29abca95217a0dceca
Edward Hibbert <edward@ehibbert.org.uk>
2018-05-23 07:40:36 +0100

    Delay loading of doodle logos until logged in and after initial page load.  This speeds up first impressions and SEO.

--- http/js/iznik/views/pages/pages.js
+++ http/js/iznik/views/pages/pages.js
@@ -212,16 +212,27 @@ define([
                     $('#bodyContent').html(template(tpl));
 
                     if (!self.modtools) {
-                        // We might have a logo override for a specific date.
-                        $.ajax({
-                            url: API + 'logo',
-                            type: 'GET',
-                            success: function (ret) {
-                                if (ret.ret == 0 && ret.hasOwnProperty('logo')) {
-                                    $('#js-homelogo').attr('src', ret.logo.path);
-                                }
+                        // We might have a logo override for a specific date.  Load it later though as it might be
+                        // large, as animated gifs tend to be.  Only load these once you're logged in to keep
+                        // the speed as observed by Google etc faster.  Slightly artificial but also...first impressions
+                        // count.
+                        self.listenToOnce(Iznik.Session, 'isLoggedIn', function (loggedIn) {
+                            if (loggedIn) {
+                                _.delay(function() {
+                                    $.ajax({
+                                        url: API + 'logo',
+                                        type: 'GET',
+                                        success: function (ret) {
+                                            if (ret.ret == 0 && ret.hasOwnProperty('logo')) {
+                                                $('#js-homelogo').attr('src', ret.logo.path);
+                                            }
+                                        }
+                                    }, 5000);
+                                });
                             }
                         });
+
+                        Iznik.Session.testLoggedIn();
                     }
 
                     $('.js-pageContent').html(self.$el);
