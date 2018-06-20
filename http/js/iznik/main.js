648fd30c00509c484ca6a8285b89cb6aafa09be2
chriscant <chris@phdcc.com>
2018-06-20 10:51:34 +0100

    Fix refresh rotate

--- http/js/iznik/main.js
+++ http/js/iznik/main.js
@@ -93,6 +93,7 @@ function hideHeaderWait(event) {
     }
 }
 window.mobileRefresh = function () {
+    console.log("mobileRefresh");
     showHeaderWait();
     Backbone.history.loadUrl();
     return false;
@@ -265,8 +266,8 @@ function mainOnAppStart() { // CC
             var self = this;
             this.errors = this.errors === undefined ? 0 : this.errors + 1;
             var thedelay = delay(this.errors);
-            //console.log("retryIt", thedelay, this, arguments);
-            console.log("retryIt", thedelay, this.responseURL); // CC
+            console.log("retryIt", thedelay, this, arguments);
+            //console.log("retryIt", thedelay, this.responseURL); // CC
             setTimeout(function () {
                 $.ajax(self);
             }, thedelay);
