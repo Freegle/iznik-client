3afa055787fd4a9aa366bc666ab871cf55729154
chriscant <chris@phdcc.com>
2018-08-07 14:17:18 +0100

    Push clearrAll() fix in plugins

--- tidydist.sh
+++ tidydist.sh
@@ -7,6 +7,9 @@ rm images/IOSLS.png
 rm images/user_ny_animated.gif
 rm images/user_ny2_animated.gif
 rm images/Firefox.png
+rm images/logos/*
+rm images/posters/*
+rm images/stories/*
 mv *.png css
 mv *.woff css
 mv *.woff2 css
