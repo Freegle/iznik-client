6c715fa222bdad8b8c6dbf1633a76a21b352cda4
Edward Hibbert <edward@ehibbert.org.uk>
2018-12-08 15:43:31 +0000

    Remove error log

--- http/js/iznik/router.js
+++ http/js/iznik/router.js
@@ -278,7 +278,6 @@ define([
                             });
                         } else {
                             require(["iznik/views/pages/user/landing"], function() {
-                                console.log("Load landing 1");
                                 var page = new Iznik.Views.User.Pages.Landing();
                                 self.loadRoute({page: page});
                             });
@@ -376,7 +375,6 @@ define([
                         }
                     } else {
                         require(["iznik/views/pages/user/landing"], function () {
-                            console.log("Load landing 2");
                             var page = new Iznik.Views.User.Pages.Landing();
                             self.loadRoute({page: page});
                         });
